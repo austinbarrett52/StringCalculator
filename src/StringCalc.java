74496d8f8eb27569e18e92167ac99f35a9801b84
Austin Barrett <austinbarrett52@gmail.com>
2018-01-29 12:06:41 -0500

    first push

---
+++ src/StringCalc.java
@@ -0,0 +1,12 @@
+
+
+public class StringCalc {
+
+    public static int Add (String numbers){
+        if (numbers == ""){
+            return 0;
+        }else{
+            return -1;
+        }
+    }
+}
